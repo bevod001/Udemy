01ce64593928f19e90838f02f7fc916520d60413
bevod001 <kreveko@gmail.com>
2022-11-10 13:54:29 +0300

    first

---
+++ script.js
@@ -0,0 +1,22 @@
+﻿let numberOffilms = +prompt('Сколько фильмов вы уже смотрели?', ''),
+    personalMoveDB = {
+        count: numberOffilms,
+        movies: {},
+        actors: {},
+        genres: [],
+        privat: false
+    },
+    unsver,
+    a,
+    b;
+
+
+
+
+
+a = prompt('Один из последний просмотренных фильмов?', '');
+personalMoveDB.movies[a] = +prompt('На сколько оцените его?', '');
+b = prompt('Один из последний просмотренных фильмов?', '');
+personalMoveDB.movies[b] = +prompt('На сколько оцените его?', '');
+
+console.log(personalMoveDB)
